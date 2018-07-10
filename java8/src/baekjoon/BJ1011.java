deaca2652e8d0d7dc8b65307544ba0fad26748e3
puzzledPublic <puzzledPublic@gmail.com>
2018-07-10 17:57:11 +0900

    algorithms

---
+++ java8/src/baekjoon/BJ1011.java
@@ -0,0 +1,43 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.StringTokenizer;
+
+//Fly me to the Alpha Centauri
+public class BJ1011 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			solve(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), bw);
+		}
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+	
+	static void solve(int a, int b, Writer w) throws IOException {
+		int len = b - a; //a부터 b까지의 거리
+		long i = 1;	//b - a < 2^31이므로 i^2일때 int범위를 넘어설 수 있으므로 long으로 선언해야한다. (int면 아래 while에서 무한루프 가능)
+		while(true) {
+			if(len <= i * i) {	//규칙을 찾아보면 i^2일때 1~i까지 워프가능하며 이는 대칭된다.(1,2,3 ... i-1, i, i-1, ... 3,2,1)			 
+				break;			
+			}
+			i++;
+		}
+		long r = 2 * i;			//(i-1)^2 < m <= i^2-i 일때 2*i-2번 워프하며 i^2-i < m <= i^2 일때 2*i-1번 워프한다.
+		if(i * i - i >= len) {
+			w.write(r - 2 + "\n");
+		}else {
+			w.write(r - 1 + "\n");
+		}
+	}
+}
---
+++ java8/src/baekjoon/BJ1475.java
@@ -0,0 +1,32 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//방 번호
+public class BJ1475 {
+	static int[] nums = new int[9];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		solve(s);
+	}
+	static void solve(String s) {
+		for(int i = 0; i < s.length(); i++) {
+			if(s.charAt(i) - '0' == 9) {	//6,9는 같다고 친다.
+				nums[6]++;
+			}else {
+				nums[s.charAt(i) - '0']++;
+			}
+		}
+		int max = 0;
+		nums[6] = nums[6] / 2 + nums[6] % 2;	//2개가 1셋트에 담기므로 2개씩 짝짓는다.
+		for(int i = 0; i < 9; i++) {
+			if(max < nums[i]) {
+				max = nums[i];
+			}
+		}
+		System.out.println(max);
+	}
+}
