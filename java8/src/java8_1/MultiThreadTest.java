ccd03edec9891e5e3ce80e99a6a91beaede13dc6
puzzledPublic <puzzledPublic@gmail.com>
2019-05-27 18:37:43 +0900

    java executor ex

--- java8/src/java8_1/MultiThreadTest.java
+++ java8/src/java8_1/MultiThreadTest.java
@@ -1,47 +1,74 @@
 package java8_1;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class MultiThreadTest {
 
-	public static void main(String args[]){
-		
-		//Thread th = new Thread(()->{for(int i=0; i < 200;i++){System.out.print(".");}});
-		//th.start();
+	public static void main(String args[]) throws InterruptedException{
+		ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
-		Scanner input = new Scanner(System.in);
-		int n = input.nextInt();
-		while(n != 'q') {
-			solve(n);
-			n = input.nextInt();
+		for(int i = 0; i < 5; i++) {
+			CompletableFuture<Void> cf = CompletableFuture.supplyAsync(() -> {
+				StringBuilder sb = new StringBuilder();
+				try {
+				URL url = new URL("https://te31.com");
+				HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+				connection.setRequestMethod("GET");
+//				connection.addRequestProperty("accept", "application/json");
+				connection.connect();
+				
+				String str = "";
+				BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+				while((str = br.readLine()) != null) {
+					sb.append(str);
+				}
+				}catch(Exception e) {
+					e.printStackTrace();
+				}
+				return sb.toString();
+			}, executor).thenAccept(System.out::println);
 		}
+		executor.shutdown();
+		executor.awaitTermination(1000, TimeUnit.MILLISECONDS);
 		
-	}
-	
-	static void solve(int n) {
-		StringBuilder sb = new StringBuilder();
-		sb.append("┌");
-		int width = n;//= n != 1 ? n * 2 : 1;
-		String w = n != 1 ? "──" : "─";
-		String s = n != 1 ? "  " : " ";
-		for(int i = 0; i < width; i++) {
-			sb.append(w);
-		}
-		sb.append("┐\n");
-		int height = n;
-		for(int i = 0; i < height; i++) {
-			sb.append("│");
-			for(int j = 0; j < width; j++) {
-				sb.append(s);
-			}
-			sb.append("│\n");
-		}
-		sb.append("└");
-		for(int i = 0; i < width; i++) {
-			sb.append(w);
-		}
-		sb.append("┘");
+//		CompletionService<String> completionService = new ExecutorCompletionService<>(executor);
 		
-		System.out.println(sb);
+//		for(int i = 0; i < 10; i++) {
+//			completionService.submit(() -> {				
+//				URL url = new URL("https://te31.com");
+//				HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+//				connection.setRequestMethod("GET");
+////				connection.addRequestProperty("accept", "application/json");
+//				connection.connect();
+//				
+//				String str = "";
+//				StringBuilder sb = new StringBuilder();
+//				BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+//				while((str = br.readLine()) != null) {
+//					sb.append(str);
+//				}
+//				return sb.toString();
+//			});
+//		}
+//		try {
+//			for(int i = 0; i < 10; i++) {
+//				Future<String> f = completionService.take();
+//				System.out.println(f.get());
+//			}
+//			executor.shutdown();
+//			executor.awaitTermination(1000, TimeUnit.MILLISECONDS);
+//		}catch(InterruptedException e) {
+//			Thread.currentThread().interrupt();
+//		}catch (ExecutionException e) {
+//			e.printStackTrace();
+//		}
+		
 	}
 }
